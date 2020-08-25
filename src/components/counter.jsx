3ed9df32daee93590742db675ebbca8478566033
FelipeL7 <felipelseven@gmail.com>
2020-08-25 08:14:58 -0300

    6- Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,26 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/200",
   };
+
+  styles = {
+    // Css propertys in camelCase notation
+    fontSize: 40,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <React.Fragment>
         {/* <span>{this.state.count}</span> */}
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <img src={this.state.imageUrl} alt="" />
+        {/* <span className="badge badge-primary m-2">{this.formatCount()}</span> */}
+
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
@@ -18,10 +31,10 @@ class Counter extends Component {
     const { count } = this.state;
 
     // Return a plain text like "Zero"
-    // return count === 0 ? "Zero" : count;
+    return count === 0 ? "Zero" : count;
 
     // We can also return a jsx expression
-    return count === 0 ? <h1>Zero</h1> : count;
+    // return count === 0 ? <h1>Zero</h1> : count;
   };
 }
 
