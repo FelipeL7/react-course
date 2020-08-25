193523c2706c26f998c5ef111f2fcc2f1a4b0984
FelipeL7 <felipelseven@gmail.com>
2020-08-25 07:14:22 -0300

    4- Specifying Children

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// Counter don't need curly braces 'cause it was defined default export
+import Counter from "./components/counter";
+
+import "./index.css";
+// import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Counter />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
