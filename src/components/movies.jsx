3ed3c5f6a7431deb1cb286a866c6f9fa7fe0776f
FelipeL7 <felipelseven@gmail.com>
2020-08-27 17:04:50 -0300

    15- Sorting- Extracting MoviesTable

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import ListGroup from "./listGroup";
+import MoviesTable from "./moviesTable";
 import { paginate } from "../utils/paginate";
 import { getGenres } from "../services/fakeGenreService";
 import { getMovies } from "../services/fakeMovieService";
@@ -72,43 +72,13 @@ class Movies extends Component {
 
         <div className="col">
           <p>Showing {count} movies in the database</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
 
-                  <td>
-                    <Like
-                      onLike={() => this.handleLike(movie)}
-                      liked={movie.liked}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-danger btn-sm"
-                      onClick={() => this.handleDelete(movie)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MoviesTable
+            movies={movies}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+          />
+
           <Pagination
             itemsCount={count}
             pageSize={pageSize}
