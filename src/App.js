f8ed163498beaf03f35499ada949fd0128bb13f0
FelipeL7 <felipelseven@gmail.com>
2020-08-28 12:58:56 -0300

    5- Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/navbar";
 import Products from "./components/products";
@@ -16,10 +16,12 @@ class App extends Component {
       <div>
         <NavBar />
         <div className="content">
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
         </div>
       </div>
     );
