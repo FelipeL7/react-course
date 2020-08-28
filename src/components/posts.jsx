737f5eb95deed98c2c5531ff64358e001f1d8bc8
FelipeL7 <felipelseven@gmail.com>
2020-08-28 15:47:10 -0300

    10- Query String Parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import queryString from "query-string";
+
 // this.props.match.params
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  // Can pass location.search ou location.hash directly
+  // This return an object based on the query string
+  const result = queryString.parse(location.search);
+  console.log(result);
   return (
     <div>
       <h1>Posts</h1>
