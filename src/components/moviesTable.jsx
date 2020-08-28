3a60fcba0d0c60d4fadacb1bbb5fadc986fd428f
FelipeL7 <felipelseven@gmail.com>
2020-08-28 09:30:05 -0300

    22- Sorting- Unique Keys - Final

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import Like from "./common/like";
 import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
+import Like from "./common/like";
 
 class MoviesTable extends Component {
   columns = [
@@ -8,8 +9,23 @@ class MoviesTable extends Component {
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
-    { key: "like" },
-    { key: "delete" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like onLike={() => this.props.onLike(movie)} liked={movie.liked} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          className="btn btn-danger btn-sm"
+          onClick={() => this.props.onDelete(movie)}
+        >
+          Delete
+        </button>
+      ),
+    },
   ];
 
   render() {
@@ -23,28 +39,12 @@ class MoviesTable extends Component {
           onSort={onSort}
         />
 
-        <tbody>
-          {movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-
-              <td>
-                <Like onLike={() => onLike(movie)} liked={movie.liked} />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => onDelete(movie)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableBody
+          columns={this.columns}
+          data={movies}
+          onLike={onLike}
+          onDelete={onDelete}
+        />
       </table>
     );
   }
